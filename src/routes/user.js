55d1756ec113faa89e2de9a8b4f04ab9af4651ae
Gustavo Isensee <gustavo.isensee@gmail.com>
2018-10-20 00:26:03 +0200

    check if user exist and create new user

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,22 +3,30 @@ const userModel = require('../database/models/user');
 const userRoute = (router) => {
   router
     .get('/user', (req, res) => {
-      userModel.find({}, (err, users) => {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        res.status(200).send(users);
-      });
+      userModel
+        .find({})
+        .then(result => {
+          res.status(200).send(result);
+        })
+        .catch(() =>
+          res.status(500).send("There was a problem finding the users.")
+        );
     })
     .post('/user', (req, res) => {
       const { name, email, password } = req.body;
-      userModel.create({
-        name,
-        email,
-        password
-      }, 
-      (err, user) => {
-        if (err) return res.status(500).send("There was a problem adding the information to the database.");
-        res.status(200).send(user);
-      });
+      userModel
+        .find({ email, password })
+        .then(result => {
+          if (result.length > 0) return res.status(409).send('User already exist.')
+
+          userModel
+            .create({ name, email, password})
+            .then(response => {
+              res.status(200).send(response);
+            })
+            .catch(() => res.status(500).send('Error creating the user'));
+        })
+        .catch(() => res.status(500).send('Error checking the user'));
     });
 };
 
