6a99535ae7feb30acb78a4c1ea69f845358c4866
Gustavo Isensee <gustavo.isensee@gmail.com>
2018-10-21 00:15:44 +0200

    Create service, httpStatus and a catchHandling

---
+++ src/constants/httpStatus.js
@@ -0,0 +1,19 @@
+const HttpObject = (code, message) => ({ code, message });
+
+const httpStatus = {
+  INFORMATIONAL: {
+    continue: HttpObject(100, 'The data is being processed.')
+  },
+  SUCCESS: {
+    ok: HttpObject(200, 'The request has succeeded.'),
+    created: HttpObject(201, 'The resource has been created.')
+  },
+  CLIENT_ERROR: {
+    conflict: HttpObject(409, 'The request could not be completed due to a conflict')
+  },
+  SERVER_ERROR: {
+    internalServerError: HttpObject(500, 'Something went wrong.')
+  }
+};
+
+module.exports = httpStatus;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,33 +1,9 @@
-const userModel = require('../database/models/user');
+const service = require('../service/user');
 
 const userRoute = (router) => {
   router
-    .get('/user', (req, res) => {
-      userModel
-        .find({})
-        .then(result => {
-          res.status(200).send(result);
-        })
-        .catch(() =>
-          res.status(500).send("There was a problem finding the users.")
-        );
-    })
-    .post('/user', (req, res) => {
-      const { name, email, password } = req.body;
-      userModel
-        .find({ email, password })
-        .then(result => {
-          if (result.length > 0) return res.status(409).send('User already exist.')
-
-          userModel
-            .create({ name, email, password})
-            .then(response => {
-              res.status(200).send(response);
-            })
-            .catch(() => res.status(500).send('Error creating the user'));
-        })
-        .catch(() => res.status(500).send('Error checking the user'));
-    });
+    .get('/user', service.getUsers)
+    .post('/user', service.createUser)
 };
 
 module.exports = userRoute;
