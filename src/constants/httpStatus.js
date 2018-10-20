646537ffb500f7094d32f5c59378887de2ce51d2
Gustavo Isensee <gustavo.isensee@gmail.com>
2018-10-21 00:48:18 +0200

    Create auth service

--- src/constants/httpStatus.js
+++ src/constants/httpStatus.js
@@ -9,7 +9,8 @@ const httpStatus = {
     created: HttpObject(201, 'The resource has been created.')
   },
   CLIENT_ERROR: {
-    conflict: HttpObject(409, 'The request could not be completed due to a conflict')
+    badRequest: HttpObject(400, 'The request has no valid data.'),
+    conflict: HttpObject(409, 'The request could not be completed due to a conflict.')
   },
   SERVER_ERROR: {
     internalServerError: HttpObject(500, 'Something went wrong.')
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,26 +1,8 @@
-const jwt = require('jsonwebtoken');
-const userModel = require('../database/models/user');
-const config = require('./../config');
+const auth = require('../service/auth');
 
 const authRoute = (router) => {
 	router
-		.post('/auth', (req, res) => {
-      const { email, password } = req.body;
-
-      userModel.find({ email, password }, (err) => {
-        if (err) return res.status(500).send("There was a problem finding the users.");
-        
-        const token = jwt.sign(
-          { email, password },
-          config.secret,
-          {
-            expiresIn: config.expiresToken
-          }
-        );
-        res.status(200).json({ token });
-      });
-    })
-
+		.post('/auth', auth)
     .get('*', (req, res, next) => {
       console.log('--- Check authentication');
       next();
