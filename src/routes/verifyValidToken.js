48d7dd3c5be1bf2be3f4e11af7c23308123265b5
Gustavo Isensee <gustavo.isensee@gmail.com>
2018-10-23 19:52:55 +0200

    Implement check valid token

--- src/routes/verifyValidToken.js
+++ src/routes/verifyValidToken.js
@@ -1,8 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+const getToken = require('../helpers/getToken');
+const { CLIENT_ERROR } = require('../constants/httpStatus');
+
 const verifyValidToken = (router) => {
   router
     .get('*', (req, res, next) => {
-      console.log('--- Check authentication');
-      next();
+      const token = getToken(req);
+      if (!token) return res
+        .status(CLIENT_ERROR.unauthorized.code)
+        .send(CLIENT_ERROR.unauthorized);
+      
+      jwt.verify(token, config.secret, (err) => {
+        if (err) return res
+          .status(CLIENT_ERROR.unauthorized.code)
+          .send(CLIENT_ERROR.unauthorized);
+        
+        next();
+      });
     });
 };
 
