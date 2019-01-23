18990e2e79d7e9f9848e82846705b78fa133160c
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-01-23 22:18:21 +0100

    Add cors

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const compression = require('compression');
+const cors = require('cors');
 const router = require('./routes');
 
 const app = express();
+app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
