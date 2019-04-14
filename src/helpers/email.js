23dace753060f02e40b158ebf22894df3702632e
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-04-14 16:26:58 +0200

    fix email

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -41,17 +41,17 @@ const sendEmail = (res, options) => {
 const getRequestResetPasswordOptions = (email, token) => ({
   to: email,
   // TODO: It needs translation #13
-  subject: 'App timer - Reset password',
+  subject: 'My Finances - Reset password',
   html: `
     <div style="font-family: Arial; font-size: 12px; width: 100%;">
-      <h3>App timer - reset password</h3><br />
-      <p>We might have lost your App timer password. Sorry for that!</p>
+      <h3>My Finances - reset password</h3><br />
+      <p>You might have lost your password. Sorry for that!</p>
       <p>No worry! You might use the following link to reset your password:</p>
       <a href="${emailConfig.pathResetPassword}?token=${token}" target="_blank">Reset your password</a><br />
       <p>If you don’t use this link within 24 hours, it will expire. To get a new password reset your password again.</p><br />
       <p style="display: grid;">
         <span>Thanks,</span>
-        <span>App timer!</span>
+        <span>My Finances!</span>
       </p>
     </div>
   `
