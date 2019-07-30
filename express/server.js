d57bb498e01494a66bd2ec4f1e71fbbf12d9807f
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-07-30 14:56:56 +0200

    Change index and create index.server

--- express/server.js
+++ express/server.js
@@ -1,16 +1,7 @@
 require('dotenv').config();
 
-const serverless = require('serverless-http');
 const app = require('./../src/app.js');
-
-// app.listen(appConfig.port, err => {
-//   if (err) {
-//     console.log(`Error: something went wrong: ${err}`);
-//     throw err;
-//   } else {
-//     console.log(`Server running on port: ${appConfig.port}`);
-//   }
-// });
+const serverless = require('serverless-http');
 
 app.use('/.netlify/functions/server', app.Router());
 
