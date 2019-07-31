1f290dbb438224a4e25491056cf7f471f5a36576
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-08-01 00:52:55 +0200

    move sentry to express/server

--- express/server.js
+++ express/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
+const Sentry = require('@sentry/node');
 const serverless = require('serverless-http');
 const { app, router } = require('./../src/app.js');
+
+Sentry.init({ dsn: 'https://d17adabdd7354e03b8fffa1488e21a02@sentry.io/1518760' });
 
 app.use('/.netlify/functions/server', router);
 
