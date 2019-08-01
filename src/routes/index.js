bfc93fb1102b4eb4887df490c80d9a325c634d22
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-08-01 16:20:46 +0200

    remove routes to test

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // Routes without authorization
-require('./account')(router);
+// require('./account')(router);
 
-// Set up a verify bearer token
-require('./verifyValidToken')(router);
+// // Set up a verify bearer token
+// require('./verifyValidToken')(router);
 
-// Routes with authorization
-require('./user')(router);
-require('./month')(router);
+// // Routes with authorization
+// require('./user')(router);
+// require('./month')(router);
 
 module.exports = router;
