1fc44cd6e3c3c664c4a6e4455e9fa4aa7c9c8900
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-08-01 16:42:34 +0200

    remove routes and add generic route to test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send('This is fine!');
+});
 // Routes without authorization
-require('./account')(router);
+// require('./account')(router);
 
 // Set up a verify bearer token
 // require('./verifyValidToken')(router);
 
 // Routes with authorization
-require('./user')(router);
-require('./month')(router);
+// require('./user')(router);
+// require('./month')(router);
 
 module.exports = router;
