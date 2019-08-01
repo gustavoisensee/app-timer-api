083ae44d525d89c6c13511608f3f170da0a9220e
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-08-01 16:02:06 +0200

    remove some modules to test

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const helmet = require('helmet');
-const compression = require('compression');
-const cors = require('cors');
+// const helmet = require('helmet');
+// const compression = require('compression');
+// const cors = require('cors');
 const router = require('./routes');
 
 const app = express();
-app.use(cors());
-app.use(helmet());
-app.use(compression());
+// app.use(cors());
+// app.use(helmet());
+// app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // app.use(router);
