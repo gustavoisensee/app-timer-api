bec2af637e973ccd6d77aa2f3c062988adb4963d
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-08-02 10:48:38 +0200

    enable cors for 2 origins

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const helmet = require('helmet');
 // const compression = require('compression');
-// const cors = require('cors');
+const cors = require('cors');
 const router = require('./routes');
 
 const app = express();
-// app.use(cors());
+app.use(cors({
+  origin: [
+    'https://my-finances-web.netlify.com',
+    'https://my-finances-web.herokuapp.com/'
+  ]
+}));
 // app.use(helmet());
 // app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
