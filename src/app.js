70c7ef9d0bf517a9960bd76b6c0ed0b8c0f1eba8
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-08-02 17:08:22 +0200

    add helmet and compression

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const helmet = require('helmet');
-// const compression = require('compression');
+const helmet = require('helmet');
+const compression = require('compression');
 const cors = require('cors');
 const router = require('./routes');
 
@@ -12,10 +12,9 @@ app.use(cors({
     'https://my-finances-web.herokuapp.com/'
   ]
 }));
-// app.use(helmet());
-// app.use(compression());
+app.use(helmet());
+app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-// app.use(router);
 
 module.exports = { app, router };
