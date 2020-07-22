40a537b86e1e33ac0b55c2fb817a11e2ad238977
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-07-22 09:48:51 +0200

    fix: cors

--- src/helpers/cors.js
+++ src/helpers/cors.js
@@ -1,11 +1,22 @@
 const cors = require('cors');
 
-const customCors = () => cors({
-  origin: [
-    'https://my-finances-web.netlify.app',
-    'http://localhost:3000',
-  ]
-});
+const whitelist = [
+  'https://my-finances-web.netlify.app',
+  'http://localhost:3000'
+];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+};
+
+const customCors = () => cors(options);
 
 
 module.exports = customCors;
