792da5a016158f1e4cbbb1e3d1d5ecbbfab7e41d
Gustavo Isensee <gustavo.isensee@gmail.com>
2020-07-22 09:39:48 +0200

    fix: cors

--- src/helpers/cors.js
+++ src/helpers/cors.js
@@ -2,8 +2,8 @@ const cors = require('cors');
 
 const customCors = () => cors({
   origin: [
-    'https://my-finances-web.netlify.com/',
-    'https://my-finances-web.netlify.app/',
+    'https://my-finances-web.netlify.app',
+    'http://localhost:3000',
   ]
 });
 
