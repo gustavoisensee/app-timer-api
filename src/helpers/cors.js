d36b98c55436cc1af108621f103f8b4c46af218c
Gustavo Isensee <gustavo.isensee@gmail.com>
2021-04-02 01:05:29 +0200

    Set new cors options

--- src/helpers/cors.js
+++ src/helpers/cors.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 const options = {
   origin: [
     'https://my-finances-web.netlify.app'
-  ]
+  ],
+  methods: ['GET', 'PUT', 'POST', 'DELETE'],
+  credentials: true
 };
 
 const customCors = () => cors(options);
