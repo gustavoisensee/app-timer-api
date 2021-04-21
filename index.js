a882c188af4962646820e5ce60a013601ba4ab51
Gustavo Isensee <gustavo.isensee@gmail.com>
2021-04-21 19:46:12 +0200

    Add  sentry

--- index.js
+++ index.js
@@ -5,10 +5,20 @@ if (process.env.NODE_ENV === 'development') {
 const port = process.env.PORT || 3000;
 const app = require('./src/app');
 const routes = require('./src/routes');
+const { initSentry, Sentry, captureException } = require('./src/helpers/sentry');
 
+initSentry();
+
+app.use(Sentry.Handlers.requestHandler());
 app.use(routes);
+app.use(Sentry.Handlers.requestHandler());
 
-app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Listening at http://localhost:${port}`);
-});
+try {
+  app.listen(port, () => {
+    // eslint-disable-next-line
+    console.log(`Listening at http://localhost:${port}`);
+  }).on('error', captureException);
+} catch (err) {
+  captureException(err);
+}
+
--- src/helpers/sentry.js
+++ src/helpers/sentry.js
@@ -1,13 +1,14 @@
 const Sentry = require('@sentry/node');
 
 const init = () => {
-  Sentry.init({ dsn: 'https://d17adabdd7354e03b8fffa1488e21a02@sentry.io/1518760' });
+  Sentry.init({ dsn: 'https://d17adabdd7354e03b8fffa1488e21a02@o285605.ingest.sentry.io/1518760' });
 };
 
 const captureMessage = (msg) => Sentry.captureMessage(msg);
 
 const captureException = (err) => Sentry.captureException(err);
 
+module.exports.Sentry = Sentry;
 module.exports.initSentry = init;
 module.exports.captureMessage = captureMessage;
 module.exports.captureException = captureException;
