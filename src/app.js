0126b7eb2feed95e66394fd659e03df07dbf1e72
Gustavo Isensee <gustavo.isensee@gmail.com>
2021-04-21 20:38:50 +0200

    Cors test

--- src/app.js
+++ src/app.js
@@ -3,13 +3,12 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const compression = require('compression');
 const cors = require('cors');
-// const corsOptions = require('./helpers/cors');
+const corsOptions = require('./helpers/cors');
 
 const app = express();
 
 // app.options('*', cors())
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.json({ limit: '50mb' }));
