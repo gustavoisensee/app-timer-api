a11871a65cfebd177b43b977d2a1ce6a9feee43b
Gustavo Isensee <gustavo.isensee@gmail.com>
2021-04-21 21:46:08 +0200

    Add dev check on cors

--- src/helpers/cors.js
+++ src/helpers/cors.js
@@ -1,8 +1,10 @@
 const whitelist = ['https://my-finances-web.netlify.app'];
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || isDev) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'))
